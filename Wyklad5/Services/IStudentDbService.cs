701d3d4bcd776fc72f7a2951b03cbb9788952ee5
s18844 <61825554+s18844@users.noreply.github.com>
2020-04-09 23:02:46 +0200

    Commit1
    Dodanie projektu

---
+++ Wyklad5/Services/IStudentDbService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wyklad5.DTOs.Requests;
+using Wyklad5.DTOs.Responses;
+
+namespace Wyklad5.Services
+{
+    public interface IStudentDbService
+    {
+        EnrollStudentResponse EnrollStudent(EnrollStudentRequest request);
+        EnrollStudentResponse PromoteStudents(int semester, string studies);
+    }
+}
